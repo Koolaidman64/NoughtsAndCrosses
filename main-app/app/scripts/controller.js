762193ff181d05d07540180e90665744ef15279b
Koolaidman64 <adam.dixon@tombola-420.local>
2015-01-13 10:03:25 +0000

    <This current build doesn't work>
    Added change player type buttons to HTML and removed commented out code
    Added temporary images to the player type changer
    Removed unneeded scopes from the controller
    Added functions to the model for the player type changer

--- main-app/app/scripts/controller.js
+++ main-app/app/scripts/controller.js
@@ -3,17 +3,21 @@ angular.module('noughtsAndCrossesApp')
     .controller('NoughtsAndCrossesController',['$scope','gameModel','gameAPI', function ($scope,gameModel,gameAPI) {
 
         $scope.gameModel = gameModel;
-        $scope.playerType1 = 'human';
-        $scope.playerType2 = 'random';
-        $scope.playerTurn = 2;
-        $scope.playerTurn = 1;
+
 
         $scope.startNewGame = function () {
-            gameAPI.startNewGame($scope.playerType1, $scope.playerType2);
+            gameAPI.startNewGame($scope.player1, $scope.player2);
         };
 
         $scope.makeMove = function (chosenSquare) {
-            gameAPI.makeMove($scope.playerTurn, chosenSquare);
+            gameAPI.makeMove($scope.gameModel.currentPlayer, chosenSquare);
         };
-    }
-]);
+
+        $scope.changePlayer1 = function () {
+            $scope.gameModel.changePlayer1();
+        };
+
+        $scope.changePlayer2 = function () {
+            $scope.gameModel.changePlayer2();
+        };
+    }]);
--- main-app/app/scripts/services/model.js
+++ main-app/app/scripts/services/model.js
@@ -7,6 +7,29 @@ angular.module('noughtsAndCrossesApp')
         this.outcome = 'continue';
         this.gameboard = '000000000';
         this.winner = 0;
+        this.player1 = 'human';
+        this.player2 = 'human';
+        this.currentPlayer = '1';
+    };
+
+    var changePlayerType = function(currentPlayer){
+        if(currentPlayer === 'human'){
+
+            return 'random';
+        }
+        if(currentPlayer === 'random'){
+
+            return 'pre-trained';
+        }
+        return 'human';
+    };
+
+    this.changePlayer1 = function(){
+        this.player1 = changePlayerType(this.player1);
+    };
+
+    this.changePlayer2 = function(){
+        this.player2 = changePlayerType(this.player2);
     };
 
     return new gameModel();
