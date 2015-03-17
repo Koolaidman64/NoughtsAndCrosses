bef5a4a4dba8f8cefc70d33ef1510e311e8230b8
Koolaidman64 <adam.dixon@tombola-567.local>
2015-03-17 12:32:47 +0000

    Added more constant to the gameModel and tests for them Added framework for gameModel tests

---
+++ main-app/tests/services/game-model-tests/game-model-tests.js
@@ -0,0 +1,16 @@
+
+(function(){
+    describe('Game model tests',function() {
+        var expect = expect.chai;
+        var gameModel;
+
+        beforeEach(function() {
+            module('tombola.noughtsAndCrosses.gameModel');
+            inject(function($injector) {
+                gameModel = $injector.get('gameModel')
+            });
+        });
+
+
+    });
+});
