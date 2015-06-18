d9552a9ef1123bd9dc5dc1e068b28e641b64bca6
Koolaidman64 <adam.dixon@tombola-567.local>
2015-06-18 10:50:30 +0100

    Added audioSprite unit test

--- main-app/tests/audio-tests/audioSpriteTests.js
+++ main-app/tests/audio-tests/audioSpriteTests.js
@@ -8,12 +8,29 @@
          audioSprite;
 
         beforeEach(function() {
-            module('tombola.noughtsAndCrosses.audioSprite');
+            module('tombola.noughtsAndCrosses.audio');
 
-            inject(function (_$injector_) {
-                $timeout = _$injector_;
+            inject(function ($injector) {
+                $timeout = $injector.get('$timeout');
+                $document = $injector.get('$document');
+                audioSprite = $injector.get('audioSprite');
             });
         });
 
+        it("Ensure audioSprite it set correctly", function() {
+            var createSprite = function() {
+
+                var doc = $document[0];
+                var body = doc.body;
+                var lastElement = body.children[body.children.length -1 ];
+                var innerElement = lastElement.children[0];
+
+                assert.equal(innerElement.localName, 'source');
+                assert.equal(lastElement.localName, 'audio');
+                assert.equal(lastElement.children.length, 1);
+            };
+
+            createSprite();
+        });
     });
 })();
